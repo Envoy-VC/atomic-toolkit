ffa1118f9cab5034beccd2a1a6c93db631cad7d7
Vedant Chainani <vedantchainani1084@gmail.com>
2023-12-17 18:17:30 +0530

    add docs

--- packages/atomic-toolkit/src/lib/config.ts
+++ packages/atomic-toolkit/src/lib/config.ts
@@ -2,6 +2,9 @@ import Arweave from 'arweave';
 
 const ArweaveClass: typeof Arweave = (Arweave as any)?.default ?? Arweave;
 
+/**
+ * Default Arweave configuration.
+ */
 export const defaultArweave = new ArweaveClass({
     host: 'arweave.net',
     port: 443,
--- packages/atomic-toolkit/src/lib/warp.ts
+++ packages/atomic-toolkit/src/lib/warp.ts
@@ -1,5 +1,16 @@
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
+/**
+ * Retries an asynchronous operation a specified number of times until it succeeds,
+ * with a delay between each attempt.
+ *
+ * @template T - The type of the result returned by the operation.
+ * @param {() => Promise<T>} operation - The asynchronous operation to be retried.
+ * @param {number} maxAttempts - The maximum number of attempts to make.
+ * @param {number} delayBetweenAttempts - The delay in milliseconds between each attempt.
+ * @returns {Promise<T>} - A promise that resolves with the result of the operation if it succeeds.
+ * @throws {Error} - If the operation fails after the maximum number of attempts.
+ */
 export const retryOperation = async <T>(
     operation: () => Promise<T>,
     maxAttempts: number,
