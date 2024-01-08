82fde4d1b0f4c9af019c81c88b52345614da3202
bobinstein <cavanaugh.stephen1991@gmail.com>
2024-01-07 22:22:43 -0500

    feat: Added function for uploading directly with Turbo

--- packages/atomic-toolkit/src/lib/upload/index.ts
+++ packages/atomic-toolkit/src/lib/upload/index.ts
@@ -1,6 +1,8 @@
 import { WebIrys } from '@irys/sdk';
+import { TurboFileFactory, DataItemOptions } from '@ardrive/turbo-sdk';
 import * as Types from '../../types/upload';
 import Mime from 'mime';
+import { Readable } from 'stream'
 
 const uploadWithIrys = async (opts: Types.IrysUploadParams) => {
     const { irys } = opts;
@@ -91,4 +93,46 @@ const uploadWithArweave = async (opts: Types.ArweaveUploadParams) => {
     }
 };
 
-export { uploadWithArweave, uploadWithIrys };
+const uploadWithTurbo = async (opts: Types.TurboUploadParams) => {
+    const { turbo, type, data, tags } = opts;
+
+    let fileStreamFactory;
+    let fileSizeFactory;
+    let dataItemOpts: DataItemOptions = {
+        tags: tags.map((tag) => ({ name: tag.name, value: tag.value })),
+    };
+
+    if (data instanceof File) {
+        // Browser environment, data is a File object
+        fileStreamFactory = () => data.stream() as unknown as Readable;
+        fileSizeFactory = () => data.size;
+    } else if (type === 'data' && typeof data === 'string') {
+        // Handling raw data as a string
+        const buffer = Buffer.from(data);
+        fileStreamFactory = () => Readable.from(buffer);
+        fileSizeFactory = () => buffer.length;
+    } else if (type === 'file' && typeof data === 'string') {
+        // Node.js environment, data is assumed to be a file path
+        const fs = require('fs');
+        fileStreamFactory = () => fs.createReadStream(data);
+        fileSizeFactory = () => fs.statSync(data).size;
+    } else {
+        throw new Error('Invalid type or data format');
+    }
+
+    const uploadParams: TurboFileFactory = {
+        fileStreamFactory,
+        fileSizeFactory,
+        dataItemOpts,
+    };
+
+    try {
+        // Perform file upload using Turbo SDK
+        const tx = await turbo.uploadFile(uploadParams);
+        return tx;
+    } catch (error) {
+        throw new Error(String(error));
+    }
+};
+
+export { uploadWithArweave, uploadWithIrys, uploadWithTurbo };
